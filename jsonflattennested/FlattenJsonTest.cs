68198c270ee05fec7f88111e71819d2c353655d3
Tobias Janssen <tobias.janssen@traperto.com>
2023-02-27 20:22:34 +0100

    fix(server): Path
    (kein Ticket)

---
+++ jsonflattennested/FlattenJsonTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using System.IO;
+
+namespace jsonflattennested;
+
+public class FlattenJsonTest
+{
+    private readonly string JsonString;
+
+    public FlattenJsonTest()
+    {
+        JsonString =  """
+{
+     "name": "John",
+     "address":{
+          "city": "Lisbon",
+          "street": "Central street"
+     }
+}
+""";
+    }
+
+    [Fact]
+    public void flatThatJsonString()
+    {
+        var flattedJsonDict = FlatNestedJson(JsonString);
+        flattedJsonDict.TryGetValue("address.city", out var value);
+
+        value.Should().NotBeNull();
+        value.Should().Be("Lisbon");
+    }
+    
+    [Fact]
+    public async Task flatThatJsonStringInFile()
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "File.json");
+        var readJson = await File.ReadAllTextAsync(filePath);
+        var flattedJsonDict = FlatNestedJson(readJson);
+        flattedJsonDict.TryGetValue("address.city", out var value);
+
+        value.Should().NotBeNull();
+        value.Should().Be("Lisbon");
+    }
+
+    private Dictionary<string, string> FlatNestedJson(string json)
+    {
+        return  JObject.Parse(json)
+                       .Descendants()
+                       .OfType<JValue>()
+                       .ToDictionary(jv => jv.Path, jv => jv.ToString(CultureInfo.InvariantCulture));
+    }
+}
